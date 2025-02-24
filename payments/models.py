9ed89fd0b917163d99b9506b52d24c285cbb0e46
bogdan <gilmutdinov.bogdazoe@gmail.com>
2025-02-24 22:47:40 +0300

    Add Item model. Register Item model in the admin panel.

--- payments/models.py
+++ payments/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=1024)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f"{self.name}, {self.price}"
